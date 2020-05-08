fb92521ddc2e19bff8e882abf329b0ea045695dd
alex k <alex.kuhn1123@gmail.com>
2020-05-08 10:39:26 -0700

    firstBadVersion js solution

--- firstBadVersion.js
+++ firstBadVersion.js
@@ -1,6 +1,6 @@
 /**
  * Definition for isBadVersion()
- *
+ * 
  * @param {integer} version number
  * @return {boolean} whether the version is bad
  * isBadVersion = function(version) {
@@ -12,10 +12,15 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function (isBadVersion) {
-  /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
-   */
-  return function (n) {};
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        while (isBadVersion(n-1)){
+            n--;
+        }
+        return n;
+    };
 };
