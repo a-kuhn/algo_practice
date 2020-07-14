84bd84116ef66840e5eeb821bb3ed74c713d4f37
alex k <alex.kuhn1123@gmail.com>
2020-07-13 19:05:25 -0700

    add auto-incrementing id to Product constructor

--- store_and_products.py
+++ store_and_products.py
@@ -8,33 +8,41 @@ class Store:
         return self
 
     def sell_product(self, id):
-        sold_product = self.products.pop(id)
+        sold_product = [p for p in self.products if p.id == id]
+        self.products = [p for p in self.products if p.id != id]
         print(">>> SELLING...")
-        sold_product.print_info()
+        sold_product[0].print_info()
         return sold_product
 
     def inflation(self, percent_increase):
         # increase price of each product
-        self.products = [p.update_price(percent_increase, True) for p in self.products]
+        self.products = [p.update_price(percent_increase, True)
+                         for p in self.products]
         return self
 
     def set_clearance(self, category, percent_discount):
-        #update all products in given category with clearance price
-        [p.update_price(percent_discount, False) for p in self.products if p.category == category]
+        # update all products in given category with clearance price
+        [p.update_price(percent_discount, False)
+         for p in self.products if p.category == category]
         return self
 
     def print_products(self):
         print(f"\n\n**********\n{self.name} currently has:\n")
         for p in self.products:
-            p.print_info() 
+            p.print_info()
         return self
 
+
 class Product:
+    next_id = 1
+
     def __init__(self, name, price, category):
+        self.id = Product.next_id
+        Product.next_id += 1
         self.name = name
         self.price = price
         self.category = category
-    
+
     def update_price(self, percent_change, is_increased):
         if is_increased:
             self.price += self.price*percent_change
@@ -43,8 +51,10 @@ class Product:
         return self
 
     def print_info(self):
-        print(f"Product name: {self.name} \nCategory: {self.category} \nPrice: {self.price}\n")
+        print(
+            f"Product name: {self.name} \nID: {self.id}\nCategory: {self.category} \nPrice: {self.price}\n")
         return self
+
 
 store1 = Store("store1")
 
