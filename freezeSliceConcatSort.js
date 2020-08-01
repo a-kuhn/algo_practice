145666c5c998b2233ba3b7daf58798027d693f05
alex k <alex.kuhn1123@gmail.com>
2020-08-01 11:12:24 -0700

    replace 'item' with 'food'

--- freezeSliceConcatSort.js
+++ freezeSliceConcatSort.js
@@ -1,25 +1,25 @@
-// Object.freeze makes object or array immutable
+//! Object.freeze makes object or array immutable
 const groceryList = Object.freeze([
-    { "item": "carrots",           "haveIngredient": false },
-    { "item": "onions",            "haveIngredient": true  },
-    { "item": "celery",            "haveIngredient": false },
-    { "item": "cremini mushrooms", "haveIngredient": false },
-    { "item": "butter",            "haveIngredient": true  }
+    { "food": "carrots",           "haveIngredient": false },
+    { "food": "onions",            "haveIngredient": true  },
+    { "food": "celery",            "haveIngredient": false },
+    { "food": "cremini mushrooms", "haveIngredient": false },
+    { "food": "butter",            "haveIngredient": true  }
   ]);
 
-const thyme = {"item":"thyme", "haveIngredient":false};
+const thyme = {"food":"thyme", "haveIngredient":false};
 
 // groceryList.push(thyme) will NOT work because freeze made groceryList immutable
-// can use spreader (...) to make copy & then pop/push/etc
+//! can use spreader (...) to make copy & then pop/push/etc
 const spreadList = [...groceryList, thyme];
 // console.log(`\nspreadList:`);
 // console.log(spreadList);
-// or .concat:
+//! or .concat:
 const concatList = groceryList.concat(thyme);
 // console.log(`\nconcatList:`);
 // console.log(concatList);
 
-// Slice takes 1-2 parameters & returns array of elements w/ indexes btwn parameters. We can follow that up with a comma and a new object. Inside the new object we can use the spread operator to copy over the attributes from the Ingredient at index 5, and overwrite its haveIngredient key to be true.
+//! Slice takes 1-2 parameters & returns array of elements w/ indexes btwn parameters. We can follow that up with a comma and a new object. Inside the new object we can use the spread operator to copy over the attributes from the Ingredient at index 5, and overwrite its haveIngredient key to be true.
 const gotTheThyme = [ ...concatList.slice(0, 5), { ...concatList[5], "haveIngredient": true } ];
 // console.log(`\ngotTheThyme:`);
 // console.log(gotTheThyme);
@@ -29,21 +29,21 @@ const notNeceCelery = [ ...gotTheThyme.slice(0, 2), ...gotTheThyme.slice(3) ];
 // console.log(`\nnotNeceCelery:`);
 // console.log(notNeceCelery);
 
-// sorting also does not work on frozen object
-const items = Object.freeze(["carrots", "onions", "celery", "mushrooms", "butter", "thyme"]);
-//items.sort(); //* this will throw an error 
+//! sorting also does not work on frozen object
+const food = Object.freeze(["carrots", "onions", "celery", "mushrooms", "butter", "thyme"]);
+//food.sort(); //* this will throw an error 
 //need to spread & then sort:
-const sortedItems = [...items].sort();
-// console.log(`\nsortedItems:`);
-// console.log(sortedItems);
+const sortedFood = [...food].sort();
+// console.log(`\nsortedFood:`);
+// console.log(sortedFood);
 
-// sorting by a key in an object:
-// if we want to sort by item, spread frozen groceryList, then call sort & pass in callback:
-const sortedGroceries = [...groceryList].sort( (a, b) => a.item > b.item );
+//! sorting by a key in an object:
+// if we want to sort by food, spread frozen groceryList, then call sort & pass in callback:
+const sortedGroceries = [...groceryList].sort( (a, b) => a.food > b.food );
 // console.log(`\nsortedGroceries:\n`);
 // console.log(sortedGroceries);
 
-// .map() used to replace for and while loops!
+//! .map() used to replace for and while loops!
 // takes 2 args: an array and a callback function
 // map passes each element in array to callback function, returning new, manipulated array
 const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
